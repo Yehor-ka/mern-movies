1b13ebf0c2e7f6fb1d684eb96614f6ce6c4cb5af
Yehor-ka <kalolg0109@gmail.com>
2022-01-14 17:21:54 +0200

    complete all api

--- api/index.js
+++ api/index.js
@@ -1,25 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const authRouter = require('./routes/auth')
-const userRouter = require('./routes/users')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users');
+const movieRouter = require('./routes/movies');
+const listRouter = require('./routes/lists');
 
-dotenv.config()
+dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log('DB Connection successfull!')).catch(err => console.log(err))
+  })
+  .then(() => console.log('DB Connection successfull!'))
+  .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth", authRouter)
-app.use("/api/users", userRouter)
-
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/movies', movieRouter);
+app.use('/api/lists', listRouter);
 
 app.listen(8800, () => {
-    console.log('Backend server is running!')
-})
+  console.log('Backend server is running!');
+});
